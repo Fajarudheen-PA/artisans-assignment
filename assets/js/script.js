f4ae07452b2ebb7f6e70ba255066a54bf4f06d1a
Fajarudheen PA <fajarudheen98@gmail.com>
2023-07-26 09:50:34 +0530

    banner updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,3 +74,35 @@ $('.bottom-section-slider-v4').slick({
         },
     ]
 });
+
+// slider
+const rangeInput = document.querySelectorAll(".banner__price-field__right__slider__range-input input"),
+    priceInput = document.querySelectorAll(".banner__price-field__right__slider__price-input input"),
+    priceValue = document.querySelectorAll(".banner__price-field__left__left-box input"),
+    progress = document.querySelector(".banner__price-field__right__slider__controller .banner__price-field__right__slider__controller__progress");
+
+let priceGap = 1000;
+
+rangeInput.forEach(input => {
+    input.addEventListener("input", e => {
+        //getting 2 values and parsing to numbers
+        let minVal = parseInt(rangeInput[0].value),
+            maxVal = parseInt(rangeInput[1].value);
+
+        if (maxVal - minVal < priceGap) {
+            if (e.target.className === "range-min") {
+                rangeInput[0].value = maxVal - priceGap;
+            }
+            else {
+                rangeInput[1].value = minVal + priceGap;
+            }
+        } else {
+            priceInput[0].value = minVal;
+            priceInput[1].value = maxVal;
+            priceValue[0].value = minVal;
+            priceValue[1].value = maxVal;
+            progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+            progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+        }
+    });
+});
